4c149513aee7b82ead02acdb29966d5d9ec83ca0
Kowcio <Kowcio>
2015-04-08 21:50:01 +0200

    Renamic files, deleting overhead, making main page pretty (basic simple navbar / foter template).

--- src/main/java/allinone/SecurityConfig.java
+++ src/main/java/allinone/SecurityConfig.java
@@ -14,37 +14,37 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-
 @Configuration
 @EnableWebMvcSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
- class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity ( prePostEnabled = true, securedEnabled = true)
+@Order ( SecurityProperties.ACCESS_OVERRIDE_ORDER)
+class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// @formatter:off
-		http
-			.authorizeRequests()
-			.antMatchers("/login").permitAll()
-			.antMatchers("/**","/all", "/console/**", "/login/**", "/css/**", "/images/**", "/js/**", "/fonts/**").permitAll()
-			.anyRequest().authenticated()
-			
-		.and()
-			.formLogin()
-			.loginPage("/login").defaultSuccessUrl("/", true).failureUrl("/login?error")
-			
-		.and()
-			.logout()
-			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-			
-		.and()
-			.exceptionHandling()
-			.accessDeniedPage("/login?error")
-			;
-		// @formatter:on
-	
-		
-}
-}
+		http.authorizeRequests().antMatchers("/login").permitAll()
+		        .antMatchers("/**", "/all", "/console/**", "/login/**", "/css/**", "/images/**", "/js/**", "/fonts/**")
+		        .permitAll().anyRequest().authenticated()
+		        .and().formLogin().loginPage("/login").defaultSuccessUrl("/", true).failureUrl("/login?error")
+		        .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		        .and().exceptionHandling().accessDeniedPage("/login?error")
 
+		        /*
+		        .jdbcAuthentication()
+				.dataSource(dataSource)
+				.withDefaultSchema()
+				.withUser("user").password("password").roles("USER").and()
+				.withUser("admin").password("password").roles("USER", "ADMIN");
+		         */
+		         
+		        ;
+		// @formatter:on
+
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+	}
+
+}
