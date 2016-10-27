3ceb423e1ef88de4570fe37e794bcbb6e39bbb95
Kowcio <Kowcio>
2016-10-27 20:28:40 +0200

    added css that shows the map !! !! !! !! !! now just mock the changing pointers for each user

--- src/main/java/allinone/service/GroupService.java
+++ src/main/java/allinone/service/GroupService.java
@@ -4,9 +4,6 @@ import allinone.repositories.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by U6041021 on 26/10/2016.
- */
 
 @Service
 public class GroupService {
---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,21 @@
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 45.518, lng: -122.672},
+    zoom: 18,
+    mapTypeId: 'satellite',
+    heading: 90,
+    tilt: 45
+
+  });
+}
+
+
+    $( document ).ready(function() {
+console.log("Moading map .... ? ");
+    initMap();
+
+    });
+
+
+
