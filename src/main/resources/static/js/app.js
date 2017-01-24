cd326daea1d330429fd5a2026eef7664ec6aa807
Kowcio <Kowcio>
2017-01-24 19:09:59 +0100

    rearranged code for better read

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,21 @@
 
+/**
+
+Initiate base function.
+
+*/
+
+    $( document ).ready(function() {
+console.log("Moading map .... ? ");
+    initMap();
+
+//init websockt
+
+
+    });
+
+
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 45.518, lng: -122.672},
@@ -9,18 +26,6 @@ function initMap() {
 
   });
 }
-
-
-    $( document ).ready(function() {
-console.log("Moading map .... ? ");
-    initMap();
-
-
-//init websockt
-
-
-
-    });
 
 
 
