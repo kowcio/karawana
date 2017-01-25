520a87dca731485bcb33ff021151afaffd8b660e
Kowcio <Kowcio>
2017-01-25 21:32:17 +0100

    fixinf asynchronous race condition on functions

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,21 +1,12 @@
 /**
 Initiate base function.
 */
+var lat,lon,map;
 
-//var x = document.getElementById("location");
-
-$( document ).ready(function() {
-
-console.log("Doc ready funcs. ");
-initMap();
-getLocation();
-//init websockt
-
-});
 
 
 function initMap() {
- var map = new google.maps.Map(document.getElementById('map'), {
+ window.map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 45.518, lng: -122.672},
     zoom: 18,
     mapTypeId: 'satellite',
@@ -34,47 +25,67 @@ function initMap() {
   });
 }
 
-
-/**
-        LOCATION for browser
-*/
-
-var x = document.getElementById("location");
 function showPosition(position) {
-    document.getElementById("location").innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
+     window.lat = parseFloat(position.coords.latitude);
+     window.lon = parseFloat(position.coords.longitude);
+         console.log(window.lat + " -x- " + window.lon);
+                  console.log(lat + " -x- " + lon);
+//       var myLatlng = new google.maps.LatLng(parseFloat(window.lat),parseFloat(window.lon));
+     $("#location").text("Latitude: " + window.lat +    "   Longitude: " + window.lon);
+
 }
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(showPosition);
     } else {
-        document.getElementById("location").innerHTML = "Geolocation is not supported by this browser.";
+        $("#location").text("Geolocation is not supported by this browser.");
     }
+
 }
+
+function moveMap(){
+    console.log(window.lat + " -ZZZ- " + window.lon);
+    window.map.setCenter(new google.maps.LatLng( 54.4136058  , 18.5827882 ));
+     var marker = new google.maps.Marker({
+        position: {lat: 54.4136058, lng: 18.5827882},
+        map: map,
+        title: 'Test !',
+         icon: {
+                path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+                strokeColor: "red",
+                scale: 3
+            }
+      });
+};
 
 
 /**
         COUNTER
 */
-var timestamp = $("countdown").text();
-timestamp /= 1000;
-$("#test").text(timestamp);
-
-function component(x, v) {
-    return Math.floor(x / v);
-}
-setInterval(function() { // execute code each second
-
-    timestamp--; // decrement timestamp with one second each second
-
-    var days    = component(timestamp, 24 * 60 * 60),      // calculate days from timestamp
-        hours   = component(timestamp,      60 * 60) % 24, // hours
-        minutes = component(timestamp,           60) % 60, // minutes
-        seconds = component(timestamp,            1) % 60; // seconds
-
-    $("#countdown").html(days + " days, HH" + hours + ":" + minutes + ":" + seconds); // display
-
-}, 1000); // interval each second = 1000 ms
+//var timestamp = $("countdown").text();
+//timestamp /= 1000;
+//$("#test").text(timestamp);
+//function component(x, v) {
+//    return Math.floor(x / v);
+//}
+//setInterval(function() { // execute code each second
+//    timestamp--; // decrement timestamp with one second each second
+//    var days    = component(timestamp, 24 * 60 * 60),      // calculate days from timestamp
+//        hours   = component(timestamp,      60 * 60) % 24, // hours
+//        minutes = component(timestamp,           60) % 60, // minutes
+//        seconds = component(timestamp,            1) % 60; // seconds
+//    $("#countdown").html(days + " days, HH" + hours + ":" + minutes + ":" + seconds); // display
+//}, 1000); // interval each second = 1000 ms
 
 
 
+$( document ).ready(function() {
+initMap();
+getLocation();
+console.log(window.lat + " -- " + window.lon);
+console.log(lat + " -- " + lon);
+
+moveMap();
+//init websockt
+
+});
