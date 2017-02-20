4793e8e1d202029f26e9559fea7b2ebb6507245b
Kowcio <Kowcio>
2017-02-20 21:12:06 +0100

    fix 16 printing profiles on op

--- src/main/java/karawana/config/ActiveProfilesLogger.java
+++ src/main/java/karawana/config/ActiveProfilesLogger.java
@@ -2,15 +2,14 @@ package karawana.config;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
 @Profile("dev")
 @Configuration
-public class ActiveProfilesLogger extends Throwable implements ApplicationListener {
+public class ActiveProfilesLogger extends Throwable implements EnvironmentAware {
     private final Logger log = Logger.getLogger(getClass());
 
     @Autowired
@@ -18,17 +17,14 @@ public class ActiveProfilesLogger extends Throwable implements ApplicationListen
 
 
     @Override
-    public void onApplicationEvent(ApplicationEvent event) {
-        //redirect na grupe ?
+    public void setEnvironment(Environment environment) {
 
         for (String s : env.getActiveProfiles())
             System.out.println(s);
         for (String s : env.getDefaultProfiles())
             System.out.println(s);
-
         System.out.println("PROFILES!!! = {}" + env.getProperty("profileActiveMaven"));
         System.out.println("PROFILES!!! = {}" + env.getProperty("activatedProperties"));
-
 
     }
 }
