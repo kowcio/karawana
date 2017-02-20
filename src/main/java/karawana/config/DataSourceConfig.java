acb617f24c9c4e2f4155d3269d61203a1d179b33
Kowcio <Kowcio>
2017-02-20 19:00:22 +0100

    fix 14 changed datasource profile

--- src/main/java/karawana/config/DataSourceConfig.java
+++ src/main/java/karawana/config/DataSourceConfig.java
@@ -13,7 +13,7 @@ import javax.servlet.ServletRegistration;
 import java.sql.SQLException;
 
 @Configuration
-@Profile("openshift")
+@Profile("dev")
 public class DataSourceConfig implements ServletContextInitializer {
     private final static Logger log = Logger.getLogger(DataSourceConfig.class);
 
