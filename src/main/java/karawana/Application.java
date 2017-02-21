e3d3da80ebefbe630cd9ccbec0b099add8151c81
Kowcio <Kowcio>
2017-02-21 23:33:22 +0100

    init fix 5

--- src/main/java/karawana/Application.java
+++ src/main/java/karawana/Application.java
@@ -8,8 +8,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -23,10 +27,26 @@ public class Application implements WebApplicationInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
     @Override
     public void onStartup(ServletContext container) {
-        SpringApplication.run(Application.class);
+
+        System.out.println("########################################### STARTUP ");
+
+        // Create the 'root' Spring application context
+        AnnotationConfigWebApplicationContext rootContext =
+                new AnnotationConfigWebApplicationContext();
+        rootContext.register(Application.class);
+
+        // Manage the lifecycle of the root application context
+        container.addListener(new ContextLoaderListener(rootContext));
+
+        // Create the dispatcher servlet's Spring application context
+        // Register and map the dispatcher servlet
+        ServletRegistration.Dynamic dispatcher =
+                container.addServlet("dispatcher", new DispatcherServlet());
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
     }
+
 
 }
