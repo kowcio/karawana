d7fafb278b1a417c936472fd2af36c292ff387c3
Kowcio <Kowcio>
2017-02-22 15:08:55 +0100

    fix 2

--- src/main/java/karawana/config/ActiveProfilesLogger.java
+++ src/main/java/karawana/config/ActiveProfilesLogger.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
-@Profile("dev")
+@Profile("openshift")
 @Configuration
 public class ActiveProfilesLogger extends Throwable implements EnvironmentAware {
     private final Logger log = Logger.getLogger(getClass());
