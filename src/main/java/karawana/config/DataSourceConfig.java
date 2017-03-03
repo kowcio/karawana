bb1e3f1a2a17f5ec5d83b4a7a543ed22d651b4cc
Kowcio <Kowcio>
2017-03-03 05:19:44 +0100

    add h2 to general build, so the class DS is on profile

--- src/main/java/karawana/config/DataSourceConfig.java
+++ src/main/java/karawana/config/DataSourceConfig.java
@@ -6,8 +6,10 @@ import org.h2.tools.Server;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class DataSourceConfig {
     private final Logger log = Logger.getLogger(getClass());
 
