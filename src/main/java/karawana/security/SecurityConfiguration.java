301718458556094d327714b64d8fc2ee8aa32994
Kowcio <Kowcio>
2019-03-27 17:18:16 +0100

    Session management with web flux redis controllers.

--- src/main/java/karawana/security/SecurityConfiguration.java
+++ src/main/java/karawana/security/SecurityConfiguration.java
@@ -1,12 +1,9 @@
 //package karawana.security;
 //
 //import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
 //
 //
 //@Configuration
@@ -16,8 +13,14 @@
 //    protected void configure(HttpSecurity http) throws Exception {
 //
 //
-//        http
-//
+//                http
+//			    .authorizeRequests()
+//                .anyRequest()
+////                        .authenticated()
+////                .and()
+////                .formLogin()
+////                .and()
+//// @formatter:on
 //
 ////        http
 ////                .csrf().disable()
@@ -25,10 +28,10 @@
 ////                .and()
 ////                .authorizeRequests()
 ////                .anyRequest()
-////                .permitAll()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-//                .sessionFixation().migrateSession()
+//                .permitAll()
+////                .sessionManagement()
+////                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+////                .sessionFixation().migrateSession()
 ////                .expiredUrl("/sessionExpired.html")
 ////                .invalidSessionUrl("/invalidSession.html");
 ////                .maxSessionsPreventsLogin(true)
@@ -40,7 +43,7 @@
 //
 ////                .invalidSessionUrl("/");
 ////                .and().csrf().disable();
-//;
+////.build();
 ////        http.headers().frameOptions().disable();
 //
 ////        http
--- src/main/java/karawana/web/controller/MainController.java
+++ src/main/java/karawana/web/controller/MainController.java
@@ -1,6 +1,5 @@
 package karawana.web.controller;
 
-import karawana.entities.BeanUser;
 import karawana.entities.User;
 import karawana.service.GroupService;
 import karawana.service.LocationService;
@@ -11,18 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
+import org.springframework.session.ReactiveSessionRepository;
+import org.springframework.session.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.WebSession;
 import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
@@ -44,40 +43,41 @@ public class MainController {
 //    @Inject
 //    BeanUser beanUser;
 
+    @Inject
+    ReactiveSessionRepository reactiveSessionRepository;
 
-    @RequestMapping(
-            value = "/"
+
+    @RequestMapping(value = "/"
 //            ,method = RequestMethod.GET
 //            ,produces = MediaType.TEXT_EVENT_STREAM_VALUE
- )
+    )
     public String mainPage(
-//            HttpSession httpSession,
-                          final Model mav
+            @RequestParam(defaultValue = "") String userName,
+            @RequestParam(defaultValue = "") String groupName,
+                WebSession session,
+            final Model mav
     ) {
-//        String user = beanUser.getUser().toString();
-//        log.info("SessionId:{}", httpSession.getId());
-//        log.info("User:{}", beanUser.getUser());
-        //if httpSession  20 min
-//        mav.addAttribute("user", beanUser.getUser());
-//        mav.addAttribute(SESSION_VAR.SESSION_ID, httpSession.getId());
-//        mav.addAttribute(SESSION_VAR.SESSION_ID, "qweasdzxc");
-//        mav.addAttribute("user", "qweasdzxc");
-
         // data streaming, data driven mode.
-
+        log.info("Session : {}", session.getId());
+        //saving a thing in session saves the session for the redis store
+        //other wise we are getting new session always and always
+        //GOLDEN LINE DO NOT DELETE
+        session.getAttributes().putIfAbsent("userName", userName);
+        log.info("Session : {}", session);
         IReactiveDataDriverContextVariable reactiveDataDrivenMode =
                 new ReactiveDataDriverContextVariable(
                         userService.findAll(), 1);
-
         mav.addAttribute("users", reactiveDataDrivenMode);
-
+        mav.addAttribute("session_static", session.getId());
+//        mav.addAttribute("session_test", note);
         return "layout";
     }
 
-
     @GetMapping(path = "/users",
             produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<User> feed() {
+    public Flux<User> feed(WebSession session
+    ) {
+        log.info("Session : {}", session.getId());
         return this.userService.findAll();
 
     }
