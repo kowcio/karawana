c670a6d83c90613c3ef05f6b555c5523bd05dd85
kp <contact@kowalskipiotr.eu>
2019-05-09 08:30:19 +0200

    merge

--- src/main/java/karawana/service/RabbitTestGeneratorMessages.java
+++ src/main/java/karawana/service/RabbitTestGeneratorMessages.java
@@ -1,10 +1,25 @@
 package karawana.service;
 
 
+import org.springframework.scheduling.annotation.Scheduled;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class RabbitTestGeneratorMessages {
+
+
+
+
+    @Scheduled(fixedRate = 5000)
+    public boolean sendGeneratedMessageToQueue(){
+
+        List<String> queuesNameForGeneration = new ArrayList<String>();
+
+        //send message to given queue
+        return true;
+
+    }
 
 
 }
