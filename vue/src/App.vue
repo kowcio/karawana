c29c9719f01a391357df3dd7c43a357b1c84ad3f
Kowcio <Kowcio>
2019-06-10 19:38:08 +0200

    basic vue map loading prototype, to be fixed from that spot

---
+++ vue/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <!-- import component with all that is needed -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+          crossorigin=""/>
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Top msg="Welcome to Your Vue.js App"/>
+    <Map></Map>
+  </div>
+</template>
+
+<script>
+
+import Top from './components/HelloWorld.vue'
+import Map from './components/Map.vue'
+import Log from './components/Log.vue'
+
+
+
+export default {
+  name: 'app',
+  components: {
+    Top,
+    Map
+
+  }
+}
+</script>
+
+
+
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#map { height: 180px; }
+</style>
---
+++ vue/src/components/Log.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="log">
+    <h1>{{log}}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Log',
+  props: {
+    log: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.log {
+  Margin-bottom:40px;
+  position:fixed;
+}
+</style>
